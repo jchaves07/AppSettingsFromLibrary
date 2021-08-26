2a6a11647d599df9332d08cd403834d80df576e0
Jorge Chaves <jorge.chaves@icloud.com>
2021-08-26 13:36:09 -0600

    Initial commit

---
+++ AppSettingsFromLibrary/AppConfig.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace AppSettingsFromLibrary
+{
+    public static class AppConfig
+    {
+        /// <summary>
+        /// Get Default ConnectionString
+        /// </summary>
+        /// <returns>Default ConnectionString</returns>
+        public static string GetConnectionString()
+        {
+            return GetConnectionString("DefaultConnection");
+        }
+        /// <summary>
+        /// Get ConnectionString by ConnectionName
+        /// </summary>
+        /// <param name="ConnectionName"></param>
+        /// <returns>ConnectionString</returns>
+        public static string GetConnectionString(string ConnectionName)
+        {
+            var configuration = GetConfiguration();
+            var result = configuration.GetSection($"ConnectionStrings:{ConnectionName}").Value;
+            return result;
+        }
+        /// <summary>
+        /// Get Configuration Root
+        /// </summary>
+        /// <returns>ConfigurationRoot</returns>
+        public static IConfigurationRoot GetConfiguration()
+        {
+            var configuration = new ConfigurationBuilder().AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json"), optional: false)
+                .AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "appsettings.Development.json"), optional: false)
+                .Build();
+            return configuration;
+        }
+    }
+}
